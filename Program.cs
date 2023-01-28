270e08a555fe7706b2c4c9829c1e264b20d9cad9
Mhabbat <119960455+Mhabbat@users.noreply.github.com>
2023-01-28 22:09:07 +0300

    Add files via upload
    Seminar6

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿  // Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных элементов массива.
-  /*
-  int[]CreateRandomArray(int size,int minValue,int maxValue)
+﻿// Необходимо написать программу,которые перевернет одномерный массив.
+/*
+
+int[]CreateRandomArray(int size,int minValue,int maxValue)
 {
     int[] array = new int [size];
     for(int i = 0; i < size; i++)
@@ -10,74 +11,6 @@
 } 
 
  
- 
- /* int [] CreateArray(int size)
-{
-    int[] array = new int [size];
-    for(int i = 0; i < size; i++)
-    {
-       Console.Write($"Input an {i + 1} element: ");
-       array[i] = Convert.ToInt32(Console.ReadLine()); 
-    }
-      
-
-      return array;  
-} */
-/*
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-    Console.Write(array[i] + " ");
-  
-Console.WriteLine();
-    }
-}
-
-int GetSumOfNegative(int[] array)
-{
-  int sum = 0;
-  for(int i = 0; i < array.Length; i++)
-  {
-    if(array[i] < 0)
-         sum += array[i];
-    
-  }
-    return sum;
-}
-
-
-Console.Write("Input a length of array: ");
-int length = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine()); 
-Console.Write("Input a max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
-    
-int [] newArray = CreateRandomArray(length, min, max);
-ShowArray(newArray);
-
-int result = GetSumOfNegative(newArray);
-Console.WriteLine("Sum of negative elements is " + result); */ 
-
-// Напишите программу замена элементов массива.Положительные элементы заменить на отицательный и наоборот. 
-/*
-  int[]CreateRandomArray(int size,int minValue,int maxValue)
-{
-    int[] array = new int [size];
-    for(int i = 0; i < size; i++)
-      array[i] = new Random().Next(minValue,maxValue + 1);
-
-      return array;
-}
-int[] GetReverse(int[] array)
-{
- for(int i = 0; i < array.Length; i++)
-    array[i]=array[i]*-1;
-return array;
-    
-}
-
 void ShowArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
@@ -89,8 +22,16 @@ Console.WriteLine();
 }
 
 
+void ReverseArray(int[] array)
+{
+    for(int i = 0, j = array.Length - 1; i < j; i++,j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 
-
+    }
+}
 Console.Write("Input a length of array: ");
 int length = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a min possible value: ");
@@ -100,105 +41,95 @@ int max = Convert.ToInt32(Console.ReadLine());
     
 int [] newArray = CreateRandomArray(length, min, max);
 ShowArray(newArray);
+ReverseArray(newArray);
+ShowArray(newArray);  */
 
-int[] result = GetReverse(newArray);
-ShowArray(result);
+// Необходимо написать программу которые на вход принимает три числа и проверяет,может ли существовать триугольник со сторонами такой длины.
+// Подсказка
+// Сумма двух строна всегда больше третей стороны.
 
-Console.WriteLine("Sum of negative elements is ");
-*/
-// Задайте массив.Напишите программу которая определяет,присутствует ли заданное число в массиве.
- /* 
-  int[]CreateRandomArray(int size,int minValue,int maxValue)
+/*
+bool Treeangle(int a,int b,int c)
 {
-    int[] array = new int [size];
-    for(int i = 0; i < size; i++)
-      array[i] = new Random().Next(minValue,maxValue + 1);
-
-      return array;
-}
-bool GetSearch(int[] array, int x)
-{
-    for (int i = 0; i < array.Length; i++)
-    { 
-      if (array[i] == x) return true;
-    }
+    if (a+b > c && b+c > a && c+a > b)
+    return true;
+    else
     return false;
-
-    
 }
+Console.WriteLine("Сторона 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сторона 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сторона 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+bool result = Treeangle(a,b,c);
+Console.WriteLine(result);   */
 
+// Необходимо не используй рекурсию вывести первое n чисел Fibonacci первое два число Fibonacci это a и b.
 
+/*int [] RandomArray(int a,int b,int size)
+{
+    int[] array = new int [size];
+    array[0] = a;
+    array[1] = b;
+    for(int i =2; i < size; i++)
+        array[i] = array[i-2] + array[i-1];
+        return array;
+}
+ 
 void ShowArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
     {
     Console.Write(array[i] + " ");
-  
-Console.WriteLine();
     }
+  
+          
+}         
+Console.WriteLine("Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Размер ряда Fibonacci: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] result = RandomArray(a,b,size);
+ShowArray(result);
+*/
+
+// Необходимо написать программу,которые будет создавать копию из одного массива с поьощью по элементного копирование.
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue) {
+  int[] array = new int[size];
+
+  for (int i = 0; i < size; i++) {
+    array[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return array;
 }
 
+void ShowArray(int[] array) {
+  for (int i = 0; i < array.Length; i++) {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
 
-
+int[] CopyArray (int[] array, int size)
+{
+  int[] newArray = new int[size];
+  for (int i = 0; i < array.Length; i++)
+    newArray[i] = array[i];
+  return newArray;
+}
 
 Console.Write("Input a length of array: ");
 int length = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine()); 
+int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a max possible value: ");
 int max = Convert.ToInt32(Console.ReadLine());
-    
-int x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input a number:  ");
-
-// Задайте одномерный массив из m случайных чисел.Найдите количество элементов массива,значения которых лежит отрезке a,b.
-/*
- int[]CreateRandomArray(int size,int minValue,int maxValue)
-{
-    int[] array = new int [size];
-    for(int i = 0; i < size; i++)
-      array[i] = new Random().Next(minValue,maxValue + 1);
-
-      return array;
-} 
-
-int GetCountFind(int[] array,int MinNum,int MaxNum)
-{
-  int count = 0;
-  for (int i = 0; i < array.Length; i++)
-      if (array[i] >= MinNum && array[i] <= MaxNum) count++;
-  return count;
-  
-}
-
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-    Console.Write(array[i] + " ");
-  
-Console.WriteLine();
-    }
-}
-Console.Write("Input a number of elements: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a min value of elements: ");
-int min = Convert.ToInt32(Console.ReadLine()); 
-Console.Write("Input a max value of elements: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Input minNumber value of elements: ");
-int MinNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input maxNumber value of elements: ");
-int MaxNum = Convert.ToInt32(Console.ReadLine());
-
-int[] newArray = CreateRandomArray(m, min, max);
+int[] newArray = CreateRandomArray(length, min, max);
 ShowArray(newArray);
-
-int result = GetCountFind(newArray,MinNum,MaxNum);
-Console.WriteLine("Количество элементов: " + result);
-
-*/
-
-
+ShowArray(CopyArray(newArray, length)); */
